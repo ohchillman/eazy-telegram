f4574a0f10adae72a35f659138d89120ae78fddf
ohchillman <ohchillman@gmail.com>
2024-10-24 15:56:42 +0300

    Added config file and examples

--- src/main/java/org/eazytg/ChatBot.java
+++ src/main/java/org/eazytg/ChatBot.java
@@ -1,4 +1,63 @@
 package org.eazytg;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.ChatJoinRequest;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class ChatBot {
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ChatBot extends TelegramLongPollingBot {
+
+    private String botUsername;
+    private String botToken;
+
+    public ChatBot() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find config.properties");
+            }
+            properties.load(input);
+            botUsername = properties.getProperty("bot.username");
+            botToken = properties.getProperty("bot.token");
+        } catch (IOException ex) {
+            Logs.error(ex.getMessage());
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage()) {
+            long chatId = update.getMessage().getChatId();
+            return;
+        }
+        if (update.hasCallbackQuery()) {
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+            return;
+        }
+        if (update.hasChatJoinRequest()) {
+            long chatId = update.getChatJoinRequest().getUserChatId();
+            return;
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
 }
--- src/main/java/org/eazytg/Main.java
+++ src/main/java/org/eazytg/Main.java
@@ -1,9 +1,31 @@
 package org.eazytg;
 
+import org.eazytg.Logs;
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.TimeZone;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws TelegramApiException {
+        ChatBot chatBot = new ChatBot();
+
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(chatBot);
+            Logs.log("ChatBot registered successfully.");
+        } catch (TelegramApiException e) {
+            Logs.error(e.getMessage());
+        }
     }
 
 }
